f289b9d811e0bf0e226218b2984d226b7b5cb983
Will Riffe <w_riffe@protonmail.com>
2023-07-28 13:58:47 -0400

    ➕ Added collapse when page selected

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/login/login';
+import Profile from './pages/profile/profile';
 import Navbar from "./components/Navbar/Navbar"
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,33 +1,40 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from "react-router-dom";
 import ReorderIcon from "@mui/icons-material/Reorder";
 import './Navbar.css';
 
 function NavBar() {
-  const [expandNavbar, setExpandNavbar] = useState(false);
+    const [expandNavbar, setExpandNavbar] = useState(false);
 
-return (
-  <div className="navbar" id={expandNavbar ? "open" : "close"}>
+    const location = useLocation();
 
-      {/* responsive accordian */}
-      <div className="toggleButton">
-          <button 
-              onClick={() => {
-                  setExpandNavbar((prev) => !prev);
-              }}>
-              <ReorderIcon />
-          </button>
-      </div>
+    {/* Here useEffect collapses accordian as user navigates pages*/}
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location])
 
-      {/* navbar links */}
-      <div className="links">
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/login">Login</Link>
-      </div>
+    return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
 
-  </div>
-)
+        {/* responsive accordian */}
+        <div className="toggleButton">
+            <button 
+                onClick={() => {
+                    setExpandNavbar((prev) => !prev);
+                }}>
+                <ReorderIcon />
+            </button>
+        </div>
+
+        {/* navbar links */}
+        <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/login">Login</Link>
+        </div>
+
+    </div>
+    )
 }
 
-export default NavBar;
+export default NavBar
