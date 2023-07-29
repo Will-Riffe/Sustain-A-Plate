f9cd9514f042e75792d2c34b69415d76d95fe67a
Bwhitman33 <bwhitman33@gmail.com>
2023-07-29 15:14:00 -0400

    Form for login console logs creds. Moving on to signup before more in depth testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/login/login';
@@ -8,13 +8,14 @@ import './App.css';
 
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={ currentForm === "login" ? <Login /> : <Signup /> } />
         </Routes>
       </Router>
     </div>);
--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,23 +1,28 @@
 import React, {useState} from 'react';
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './login.css';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPass] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email);
   }
 
   return (
+    <>
     <form onSubmit={handleSubmit}>
       <label forhtml= "email">email</label>
-      <input type= 'email' placeholder='example@email.com' id='email' name='email' />
+      <input value={email} onChange={(e) => setEmail(e.target.value)} 
+       type= 'email' placeholder='example@email.com' id='email' name='email' />
       <label forhtml= "password">Password</label>
-      <input type= 'password' placeholder='***********' id='password' name='password' />
-      <button>Log In!</button>
+      <input value={password} onChange={(e) => setPass(e.target.value)} 
+       type= 'password' placeholder='***********' id='password' name='password' />
+      <button type='submit'>Log In!</button>
     </form>
+    <button>Don't have an account? Register here.</button>
+    </>
   )
 }
