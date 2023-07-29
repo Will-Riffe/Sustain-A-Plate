17cf348743cd51d122c9ffe3f7bd81e2933a6576
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-07-28 23:11:27 -0400

    import jwt auth, sign token

--- server/utils/userRoutes.js
+++ server/utils/userRoutes.js
@@ -1,19 +1,22 @@
 // util/userRoutes.js
-const bcrypt = require('bcryptjs');
-const express = require('express');
-const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+const express = require("express");
+const User = require("../models/user");
+const { signToken } = require("./auth");
 
 const router = express.Router();
 
 // Registration route
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
     // Check if the username or email already exists
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
     if (existingUser) {
-      return res.status(400).json({ message: 'Username or email already exists' });
+      return res
+        .status(400)
+        .json({ message: "Username or email already exists" });
     }
 
     // Hash the password before saving it to the database
@@ -28,37 +31,38 @@ router.post('/register', async (req, res) => {
 
     await newUser.save();
 
-    return res.status(201).json({ message: 'User registered successfully' });
+    return res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
-    console.error('Error during user registration:', err);
-    return res.status(500).json({ message: 'Internal server error' });
+    console.error("Error during user registration:", err);
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 
 // Login route
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
 
     // Check if the user exists in the database
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     // Compare the provided password with the hashed password in the database
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     // If the password is valid, create and send a JWT token to the client for authentication
     // We'll implement this part later
+    const token = signToken(user);
 
-    return res.status(200).json({ message: 'Login successful' });
+    return res.status(200).json({ message: "Login successful" });
   } catch (err) {
-    console.error('Error during user login:', err);
-    return res.status(500).json({ message: 'Internal server error' });
+    console.error("Error during user login:", err);
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 
