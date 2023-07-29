cd11c299e90189c2fe6f8b37df4e4ada36678513
Will Riffe <w_riffe@protonmail.com>
2023-07-29 14:18:37 -0400

    🔧 Adjusted accordian ✏️ Added Title

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import ReorderIcon from "@mui/icons-material/Reorder";
 import './Navbar.css';
 
 function NavBar() {
+    
     const [expandNavbar, setExpandNavbar] = useState(false);
-
     const location = useLocation();
 
     {/* Here useEffect collapses accordian as user navigates pages*/}
@@ -14,8 +14,13 @@ function NavBar() {
     }, [location])
 
     return (
-    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+    <div className="navbar">
 
+        {/* Centered h3 element */}
+        <div className="centered-content">
+            <h3>Sustain-A-Plate</h3>
+        </div>
+        
         {/* responsive accordian */}
         <div className="toggleButton">
             <button 
@@ -26,12 +31,14 @@ function NavBar() {
             </button>
         </div>
 
+
         {/* navbar links */}
-        <div className="links">
+        <div className={`links ${expandNavbar ? "expanded" : ""}`}>
             <Link to="/">Home</Link>
             <Link to="/profile">Profile</Link>
             <Link to="/login">Login</Link>
         </div>
+
 
     </div>
     )
