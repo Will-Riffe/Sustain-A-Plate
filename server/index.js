9aec79bedc60f18cbd6db4eb7238d53256729ba1
Will Riffe <w_riffe@protonmail.com>
2023-08-02 19:21:50 -0400

    HEROKU TEST 3

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const server = new ApolloServer({
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, "../client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("/", (req, res) => {
