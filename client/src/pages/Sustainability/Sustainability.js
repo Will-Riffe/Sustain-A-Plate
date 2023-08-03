6d9fc14ae9851072c6d284ea9c6ab826239856ab
mmoghal <mmoghal29@gmail.com>
2023-08-02 21:25:49 -0400

    Added Sustainability Page

---
+++ client/src/pages/Sustainability/Sustainability.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Sustainability = () => {
+  return (
+    <div className="sustainability-container">
+      <h2>Sustainability Tips</h2>
+      {/* Add your sustainability tips and educational content here */}
+      <p>
+        1. Reduce Food Waste: Plan your meals and buy only what you need to minimize food waste. Consider composting food scraps to reduce landfill waste.
+      </p>
+      <p>
+        2. Choose Local and Seasonal Foods: Support local farmers and reduce the carbon footprint of your meals by choosing foods that are in season and locally produced.
+      </p>
+      <p>
+        3. Opt for Plant-Based Meals: Incorporate more plant-based foods into your diet to reduce the environmental impact of meat production.
+      </p>
+      <p>
+        4. Use Reusable Containers and Utensils: Say no to single-use plastics by using reusable containers and utensils for your meals and snacks.
+      </p>
+      <p>
+        5. Conserve Water: Be mindful of water usage while cooking and cleaning. Fix leaks and opt for water-saving practices.
+      </p>
+      <p>
+        6. Support Sustainable Fishing: Choose seafood from sustainable sources to protect marine ecosystems and fish populations.
+      </p>
+      <p>
+        7. Compost Food Scraps: Composting food scraps reduces landfill waste and creates nutrient-rich compost for your garden.
+      </p>
+      <p>
+        8. Shop Smart: Be a conscious consumer by supporting brands and products that prioritize sustainability and eco-friendly practices.
+      </p>
+      <p>
+        9. Educate Others: Share your sustainability knowledge with friends and family to promote awareness and collective action.
+      </p>
+      <p>
+        10. Advocate for Change: Get involved in local initiatives and advocate for policies that promote sustainable food systems and reduce food waste.
+      </p>
+    </div>
+  );
+};
+
+export default Sustainability;
