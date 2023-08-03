6ad4385696afe486d7dc8070da74d35475effe79
Bwhitman33 <bwhitman33@gmail.com>
2023-08-02 20:24:07 -0400

    Profile link navs work. Thanks again John!

--- client/src/components/profileCards/profileCards.js
+++ client/src/components/profileCards/profileCards.js
@@ -1,17 +1,18 @@
 import React from "react";
-import './profileCards.css'
+import './profileCards.css';
+import { Link } from "react-router-dom";
 
 export default function ProfileCards ({ title, image, link }) {
 
     return (
         <div className="profile-card" key={title}>
-            <a href={link}>
+            < Link to={ link } >
                 <img
                     src={image}
                     alt={title}
                     className="card-icon"
                 />    
-            </a>
+                </Link>
             <div className="icon-name">
                 <h3> {title} </h3>
             </div>
