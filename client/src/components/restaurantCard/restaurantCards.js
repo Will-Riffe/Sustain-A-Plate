fba412f5f50463a68ae4fb385d04a4bcb33ef576
Bwhitman33 <bwhitman33@gmail.com>
2023-08-04 17:01:02 -0400

    added proper Link codes to restaurant cards for deployed app

--- client/src/components/restaurantCard/restaurantCards.js
+++ client/src/components/restaurantCard/restaurantCards.js
@@ -1,17 +1,18 @@
 import React from "react";
-import './restaurantCards.css'
+import './restaurantCards.css';
+import { Link } from "react-router-dom";
 
 export default function RestaurantCards ({ title, image, link }) {
 
     return (
         <div className="restaurant-card" key={title}>
-            <a href={link}>
+            < Link to = {link}>
                 <img
                     src={image}
                     alt={title}
                     className="restcard-icon"
                 />    
-            </a>
+            </Link>
             <div className="resticon-name">
                 <h3> {title} </h3>
             </div>
