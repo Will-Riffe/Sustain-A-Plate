cb212aa3ea14676128e5e43f445578309f11c28f
Bwhitman33 <bwhitman33@gmail.com>
2023-08-05 15:40:23 -0400

    Added Claim Inventory buttons to shoprite.js. Event handler doesnt work, but need to step away for an hour or two. Will come back to this later

--- client/src/pages/shoprite/shoprite.js
+++ client/src/pages/shoprite/shoprite.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_FOODLISTINGS_BY_STORE } from '../../utils/queries';
+import { useMutation } from '@apollo/client';
+import { UPDATEFOODLISTING } from '../../utils/mutations';
+import { DELETEFOODLISTING } from '../../utils/mutations';
 
-function Shoprite() {
+export default function Shoprite() {
   const { loading, error, data } = useQuery(QUERY_FOODLISTINGS_BY_STORE, {
     variables: {
       donorId: "64cd7cead3c47624c798b7c4"
     }
   });
+
+  const [foodItem, SelectFoodItem] = useState('');
+  const [quantity, SelectQuantity] = useState('');
+  const [isClaimed, SelectIsClaimed] = useState('');
+
+  const [UpdateFoodListing] = useMutation(UPDATEFOODLISTING);
+  const [DeleteFoodListing] = useMutation(DELETEFOODLISTING); 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const mutationResponse = await UpdateFoodListing({
+        variables: {
+          input: {
+            foodItem: foodItem,
+            quantity: quantity,
+            isClaimed: isClaimed
+          },
+        },
+      });
+
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
@@ -16,13 +45,14 @@ function Shoprite() {
     <div>
       <h2>Food Listings</h2>
       {data.foodListingsByDonorId.map((listing) => (
-        <div key={listing._id}>
+        <div key={listing._id} onSubmit={handleSubmit}>
           <p>donor Id:{listing.donorId}</p>
           <p>Food Item: {listing.foodItem}</p>
           <p>Description: {listing.description}</p>
           <p>Expiry Date: {listing.expiryDate}</p>
           <p>Quantity: {listing.quantity}</p>
           <p>Is Claimed: {listing.isClaimed ? 'Yes' : 'No'}</p>
+          <button type="submit">Claim Inventory</button>
           <hr />
         </div>
       ))}
@@ -30,4 +60,4 @@ function Shoprite() {
   );
 }
 
-export default Shoprite;
+
