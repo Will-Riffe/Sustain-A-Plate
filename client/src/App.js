3cdfe337ca04d2b6bf4f9d3ab78d7d5ebe115d52
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-08-06 21:28:41 -0400

    added redirects to login page when attempting to view inventory without being logged in

--- client/src/App.js
+++ client/src/App.js
@@ -11,10 +11,9 @@ import Restaurants from "./pages/restaurants/restaurants";
 import Sustainability from "./pages/Sustainability/Sustainability"; // Imported the Sustainability component
 import Contact from "./pages/contact/contact";
 import RestaurantCards from "./components/restaurantCard/restaurantCards";
-import FoodListings from "./components/foodListings/foodListings"; // Update the import path based on the actual location of donorInventory.js
+import FoodListings from "./components/foodListings/foodListings";
 import Inventory from "./pages/Inventory/Inventory";
 import About from "./pages/About";
-
 
 import "./App.css"; // DO NOT DELETE ;P
 import {
@@ -24,7 +23,6 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import DonorInventory from "./pages/donorInventory/donorInventory";
 
 const httpLink = createHttpLink({
   uri: process.env.MONGODB_URI, // Replace with your GraphQL server endpoint
@@ -75,10 +73,8 @@ function App() {
               <Route path="/contact" element={<Contact />} />
               <Route path="/Sustainability" element={<Sustainability />} />
               <Route path="/foodListings" element={<FoodListings />} />
-              <Route path="/donorInventory" element={<DonorInventory />} />
               <Route path="/:donorName" element={<Inventory />} />
               <Route path="/about" element={<About />} />
-
             </Routes>
           </div>
           <RestaurantCards />{" "}
--- client/src/pages/profile/profile.js
+++ client/src/pages/profile/profile.js
@@ -1,45 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import transHistory from '../../components/transactions';
-import PROFILEPICS from '../../assets/profile-templates/profilepics';
-import ProfileCards from '../../components/profileCards/profileCards';
+import PROFILEPICS from "../../assets/profile-templates/profilepics";
+import ProfileCards from "../../components/profileCards/profileCards";
 import "./profile.css";
+import AuthService from "../../utils/auth";
 
 export default function Profile() {
-  const [profile] = useState([
-    {
-      title: 'Browse Stores',
-      image: PROFILEPICS.Browse,
-      link: '/food'
-    },
-    {
-      title: 'Transaction History',
-      image: PROFILEPICS.PreviousTransactions,
-      link: '/list'
-    }
-  ])
-  return (
-    <section>
-      <div className="lead-text">
-        <h3>
-          <em>
-            You're Logged In!
-          </em>
-        </h3>
-        <p className="instructions">Full site navigation is open above and below!</p>
-      </div>
-      <div></div>
-      <div className='profileCard-sections'>
-        {profile.map((profilecard, index) => (
-          <ProfileCards key={index} {...profilecard} />
-        ))}
-      </div>
-      <style jsx>{`
-        img {
-          width: 15vw;
-          height: auto;
-          margin-top:5vh;
-        }
-      `}</style>
-    </section>
-  )
+  if (AuthService.loggedIn()) {
+    window.location.replace("/login");
+    return;
+  } else if (!AuthService.loggedIn()) {
+    const [profile] = useState([
+      {
+        title: "Browse Stores",
+        image: PROFILEPICS.Browse,
+        link: "/food",
+      },
+      {
+        title: "Transaction History",
+        image: PROFILEPICS.PreviousTransactions,
+        link: "/list",
+      },
+    ]);
+    return (
+      <section>
+        <div className="lead-text">
+          <p className="instructions">
+            Full site navigation is open above and below!
+          </p>
+        </div>
+        <div></div>
+        <div className="profileCard-sections">
+          {profile.map((profilecard, index) => (
+            <ProfileCards key={index} {...profilecard} />
+          ))}
+        </div>
+        <style jsx>{`
+          img {
+            width: 15vw;
+            height: auto;
+            margin-top: 5vh;
+          }
+        `}</style>
+      </section>
+    );
+  }
 }
